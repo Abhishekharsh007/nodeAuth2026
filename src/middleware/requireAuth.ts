684e125c9f851b399019e393bbcc88dc695213a9
Abhishekharsh007 <abhishekharsh3411@gmail.com>
2026-09-10 21:07:57 +0530

    feat: add requireAuth middleware to enforce authentication on protected routes

---
+++ src/middleware/requireAuth.ts
@@ -0,0 +1,48 @@
+import { Request, Response, NextFunction } from "express";
+import { verifyAccessToken } from "../lib/token";
+import { User } from "../models/user.models";
+import { email } from "zod";
+
+async function requireAuth(req: Request, res: Response, next: NextFunction) { 
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) { 
+        return res.status(401).json({
+            message: 'You are not auth user! You cant enter the building.'
+        });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    try {
+        const payload = verifyAccessToken(token);
+
+        const user = await User.findById(payload.sub);
+
+        if (!user) {
+            return res.status(401).json({
+                message: 'User not found! You cant enter the building.'
+            });
+        }
+
+        if (user.tokenVersion !== payload.tokenVersion) { 
+            return res.status(401).json({ message: 'Token invalidated!' });
+        }
+
+        const authReq = req as any;
+
+        authReq.user = {
+            id: user.id,
+            email: user.email,
+            name: user.name,
+            role: user.role,
+            isEmailVerified: user.isEmailVerified
+        };
+
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: 'Invalid Token in requireAuth middleware' });
+    }
+}
+
+export default requireAuth;
