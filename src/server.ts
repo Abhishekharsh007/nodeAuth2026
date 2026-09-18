497d7fb138a420fc2bc83249c49f78ab5d8edc59
Abhishekharsh007 <abhishekharsh3411@gmail.com>
2026-09-18 11:03:14 +0530

    refactor: simplify server startup log message

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ async function startServer() {
     const server = http.createServer(app);
 
     server.listen(process.env.PORT, () => { 
-        console.log(`Server is running on http://localhost:${process.env.PORT}`);
+        console.log(`Server is running!`);
     });
 }
 
